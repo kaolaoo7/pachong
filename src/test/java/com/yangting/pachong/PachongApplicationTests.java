d11fa391d023da8c2224dbf7642f34f378f30b9f
kaolaoo7 <406763876@qq.com>
2020-01-22 15:14:26 +0800

    Initial commit

---
+++ src/test/java/com/yangting/pachong/PachongApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yangting.pachong;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PachongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}
